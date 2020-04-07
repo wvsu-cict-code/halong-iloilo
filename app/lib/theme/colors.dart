908d41e679b1e10c3c2b56e612b9d25c3ced0f58
mjsolidarios <bashdev7@gmail.com>
2020-04-07 12:03:01 +0800

    Initial commit with theming data

---
+++ app/lib/theme/colors.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+Map<int, Color> peacefulPastures = {
+  50: Color.fromRGBO(148, 211, 172, .1),
+  100: Color.fromRGBO(148, 211, 172, .2),
+  200: Color.fromRGBO(148, 211, 172, .3),
+  300: Color.fromRGBO(148, 211, 172, .4),
+  400: Color.fromRGBO(148, 211, 172, .5),
+  500: Color.fromRGBO(148, 211, 172, .6),
+  600: Color.fromRGBO(148, 211, 172, .7),
+  700: Color.fromRGBO(148, 211, 172, .8),
+  800: Color.fromRGBO(148, 211, 172, .9),
+  900: Color.fromRGBO(148, 211, 172, 1),
+};
+
+Map<int, Color> oswegoTea = {
+  50: Color.fromRGBO(101, 92, 86, .1),
+  100: Color.fromRGBO(101, 92, 86, .2),
+  200: Color.fromRGBO(101, 92, 86, .3),
+  300: Color.fromRGBO(101, 92, 86, .4),
+  400: Color.fromRGBO(101, 92, 86, .5),
+  500: Color.fromRGBO(101, 92, 86, .6),
+  600: Color.fromRGBO(101, 92, 86, .7),
+  700: Color.fromRGBO(101, 92, 86, .8),
+  800: Color.fromRGBO(101, 92, 86, .9),
+  900: Color.fromRGBO(101, 92, 86, 1),
+};
+
+Map<int, Color> celeryStick = {
+  50: Color.fromRGBO(204, 237, 210, .1),
+  100: Color.fromRGBO(204, 237, 210, .2),
+  200: Color.fromRGBO(204, 237, 210, .3),
+  300: Color.fromRGBO(204, 237, 210, .4),
+  400: Color.fromRGBO(204, 237, 210, .5),
+  500: Color.fromRGBO(204, 237, 210, .6),
+  600: Color.fromRGBO(204, 237, 210, .7),
+  700: Color.fromRGBO(204, 237, 210, .8),
+  800: Color.fromRGBO(204, 237, 210, .9),
+  900: Color.fromRGBO(204, 237, 210, 1),
+};
+
+Map<int, Color> limeSlice = {
+  50: Color.fromRGBO(239, 252, 239, .1),
+  100: Color.fromRGBO(239, 252, 239, .2),
+  200: Color.fromRGBO(239, 252, 239, .3),
+  300: Color.fromRGBO(239, 252, 239, .4),
+  400: Color.fromRGBO(239, 252, 239, .5),
+  500: Color.fromRGBO(239, 252, 239, .6),
+  600: Color.fromRGBO(239, 252, 239, .7),
+  700: Color.fromRGBO(239, 252, 239, .8),
+  800: Color.fromRGBO(239, 252, 239, .9),
+  900: Color.fromRGBO(239, 252, 239, 1),
+};
+
+MaterialColor primaryColor = MaterialColor(0xFF94D3AC, peacefulPastures);
+MaterialColor secondaryColor = MaterialColor(0xFF655C56, oswegoTea);
+MaterialColor lightShadeColor = MaterialColor(0xFFCCEDD2, celeryStick);
+MaterialColor washoutColor = MaterialColor(0xFFEFFCEF, limeSlice);
